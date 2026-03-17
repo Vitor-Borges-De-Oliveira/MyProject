358c9b8af691519d6ae3a35f9229856cb078b72a
Vitor Borges de Oliveira <vitorborgesdeoliveira2308@gmail.com>
2026-03-16 21:20:30 -0300

    mais coisa 2

--- atividades-teste-de-mesa.py/exercicio2.py
+++ atividades-teste-de-mesa.py/exercicio2.py
@@ -9,5 +9,27 @@ estado = input("Estado: ")
 trat = input("Tratamento: ")
 libera = input("Data de liberação: ")
 
-dados = nome,  nasc, sexo, diag, estado, trat, libera
+print("\n1 - Ver dados")
+print("2 - Alterar dados")
 
+opcao = int(input("Digite uma opção: "))
+
+if opcao == 1:
+    print("\nDados do paciente: ")
+    print(f"Nome: {nome}")
+    print(f"Data de nascimento: {nasc}")
+    print(f"Sexo: {sexo}")
+    print(f"Diagnóstico: {diag}")
+    print(f"Estado: {estado}")
+    print(f"Tratamento: {trat}")
+    print(f"Data de liberação: {libera}")
+elif opcao == 2:
+    nome = input("Nome: ")
+    nasc= input("Data de nascimento: ")
+    sexo = input("Sexo: ")
+    diag = input("Diagnóstico: ")
+    estado = input("Estado: ")
+    trat = input("Tratamento: ")
+    libera = input("Data de liberação: ")
+else:
+    print("Por favor, digite uma opção válida.")
