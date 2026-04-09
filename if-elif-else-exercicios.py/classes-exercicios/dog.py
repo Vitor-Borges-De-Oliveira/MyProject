fef483d4726ff64d718d60470e611b02e15016a2
Vitor Borges de Oliveira <vitorborgesdeoliveira2308@gmail.com>
2026-04-08 21:00:05 -0300

    classes e um html teste

---
+++ if-elif-else-exercicios.py/classes-exercicios/dog.py
@@ -0,0 +1,22 @@
+# Exercício 1, dia 08/04/2026
+
+"""
+Em seu editor favorito, complete os seguintes passos:
+
+¨ Crie uma classe chamada `Dog`
+¨ Adicione um método `__init__` com parâmetros `name` e `age`, e armazene-os como propriedades usando `self`
+¨ Adicione um método chamado `bark ` que imprime o nome do cachorro seguido de "diz Woof!"
+¨ Crie um objeto `d1` da classe `Dog` com o nome "Buddy" e idade 3
+¨ Chame o método `bark` em d1
+"""
+
+class Dog:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def bark(self):
+        print(f"{self.name} diz Woof!")
+
+d1 = Dog("Buddy", 3)
+d1.bark()
